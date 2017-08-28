45d6b49de453217cc899f6458a73afeac0a2ffa9
booneng <ohbooneng@gmail.com>
2017-08-28 20:48:23 +0800

    SFD

--- web/restapi/__init__.py
+++ web/restapi/__init__.py
@@ -21,39 +21,6 @@ cache = redis.StrictRedis(
     host = "10.148.0.4",
     port = "6379",
 )
-
-country_retailers = {}
-
-def collect():
-    scrapper_keys = cache.keys('Counter/Scrapper*Total')
-    for key in scrapper_keys:
-        data = {}
-        country_retailer = re.findall(r'Counter/Scrapper(.*?)/Total', str(key))[0]
-        count = int(cache.get(key))
-        current_time = datetime.datetime.now()
-        data['count'] = count
-        if country_retailer not in country_retailers:
-            data['start_time'] = current_time
-            data['count'] = count
-            data['stop_time'] = None
-            country_retailers[country_retailer] = data
-            continue
-        old_data = country_retailers[country_retailer]
-        old_count = old_data['count']
-        old_start_time = old_data['start_time']
-        old_stop_time = old_data['stop_time']
-        if count < old_count:
-            data['start_time'] = current_time
-            data['stop_time'] = None
-        elif old_stop_time == None:
-            if count == old_count:
-                data['start_time'] = old_start_time
-                data['stop_time'] = current_time
-        country_retailers[country_retailer] = data
-    cache.set('scrapper_time_metrics', country_retailers)
-    
-    
-    #pickle.dump(country_retailers, open(country_retailers_pickle_path, "wb" ))  
     
 def help_decorator(f):
     """
@@ -79,12 +46,7 @@ def index():
 @app.route('/times'
            '')
 def times():
-    #country_retailers_pickle_path = '/usr/src/app/web/static/country_retailers.p'    
-    #try:
-        #country_retailers = pickle.load(open(country_retailers_pickle_path, "rb"))
-    #except (OSError, IOError) as e:
-        #country_retailers = {'test':{'start_time': datetime.datetime.now(), 'error': e}}
-    collect()
+    country_retailers = loads(cache.get('scrapper_time_metrics'))
     data = []
     for cr_key in country_retailers:
         country_retailer = country_retailers[cr_key]
