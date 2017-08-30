13f0b72188a46844a9727c7580d31670f2365027
booneng <ohbooneng@gmail.com>
2017-08-30 17:09:21 +0800

    FS

--- web/restapi/__init__.py
+++ web/restapi/__init__.py
@@ -12,6 +12,7 @@ import datetime
 from datetime import timedelta
 import pickle
 import threading
+from pytz import timezone
 
 #country_retailers_pickle_path = '/usr/src/app/web/static/country_retailers.p'
 
@@ -55,4 +56,33 @@ def times():
         if error:
             cr_data['duration'] = error
         data.append(cr_data)
+    return render_template('index2.html', **locals())
+
+def times():
+    country_retailers = loads(cache.get('scrapper_time_metrics').decode('utf-8'))
+    finished_today = []
+    still_running_today = []
+    has_not_started_today = []
+    for cr_key in country_retailers:
+        country_retailer = country_retailers[cr_key]
+        start_time = datetime.datetime.strptime(country_retailer['start_time'], "%Y-%m-%d %H:%M:%S")
+        stop_time = country_retailer.get('stop_time', None)
+        tz = timezone('Asia/Singapore')
+        todays_date = datetime.datetime.now(tz).date()
+        error = country_retailer.get('error', None)
+        cr_data = {}
+        cr_data['key'] = cr_key
+        cr_data['date'] = str(start_time.date())
+        cr_data['start_time'] = str(start_time.time())
+        if stop_time:
+            stop_time = datetime.datetime.strptime(country_retailer['stop_time'], "%Y-%m-%d %H:%M:%S")
+            cr_data['stop_time'] = str(stop_time.time())
+            cr_data['duration'] = str(stop_time - start_time)        
+        if todays_date == start_time.date():
+            if stop_time:
+                finished_today.append(cr_data)
+            else:
+                still_running_today.append(cr_data)
+        else:
+            has_not_started_today.append(cr_data)
     return render_template('index2.html', **locals())
