e3c41acde06f58d3b02f6f84bf275d08346b11ab
booneng <ohbooneng@gmail.com>
2017-08-31 16:36:02 +0800

    fix error

--- web/restapi/refresh_data.py
+++ web/restapi/refresh_data.py
@@ -19,33 +19,36 @@ def collect():
         country_retailers = {}
     scrapper_keys = cache.keys('Counter/Scrapper*Total')
     for key in scrapper_keys:
-        data = {}
-        country_retailer = re.findall(r'Counter/Scrapper(.*?)/Total', str(key))[0]
-        count = int(cache.get(key))
-        tz = timezone('Asia/Singapore')
-        current_time = str(datetime.datetime.now(tz))[0:19]
-        data['count'] = count
-        if country_retailer not in country_retailers:
-            data['start_time'] = current_time
+        try:
+            data = {}
+            country_retailer = re.findall(r'Counter/Scrapper(.*?)/Total', str(key))[0]
+            count = int(cache.get(key))
+            tz = timezone('Asia/Singapore')
+            current_time = str(datetime.datetime.now(tz))[0:19]
             data['count'] = count
-            data['stop_time'] = None
+            if country_retailer not in country_retailers:
+                data['start_time'] = current_time
+                data['count'] = count
+                data['stop_time'] = None
+                country_retailers[country_retailer] = data
+                continue
+            old_data = country_retailers[country_retailer]
+            old_count = old_data['count']
+            old_start_time = old_data['start_time']
+            old_stop_time = old_data['stop_time']
+            if count < old_count:
+                data['start_time'] = current_time
+                data['stop_time'] = None
+            elif count > old_count:
+                data['start_time'] = old_start_time
+                data['stop_time'] = None
+            else:
+                data['start_time'] = old_start_time
+                data['stop_time'] = old_stop_time if old_stop_time else current_time
+                    
             country_retailers[country_retailer] = data
-            continue
-        old_data = country_retailers[country_retailer]
-        old_count = old_data['count']
-        old_start_time = old_data['start_time']
-        old_stop_time = old_data['stop_time']
-        if count < old_count:
-            data['start_time'] = current_time
-            data['stop_time'] = None
-        elif count > old_count:
-            data['start_time'] = old_start_time
-            data['stop_time'] = None
-        else:
-            data['start_time'] = old_start_time
-            data['stop_time'] = old_stop_time if old_stop_time else current_time
-                
-        country_retailers[country_retailer] = data
+        except Exception as e:
+            print(e)
     cache.set('scrapper_time_metrics', json.dumps(country_retailers))
    
     
