c9dd7fbc7dded0de4164a6d7fa3235d38ede02ec
booneng <ohbooneng@gmail.com>
2017-10-27 12:07:13 +0800

    staging and production

--- web/restapi/__init__.py
+++ web/restapi/__init__.py
@@ -13,7 +13,6 @@ from datetime import timedelta
 import pickle
 import threading
 from pytz import timezone
-
 #country_retailers_pickle_path = '/usr/src/app/web/static/country_retailers.p'
 
 app = Flask(__name__)
@@ -36,9 +35,45 @@ def help_decorator(f):
 
 app.debug = True
 
-@app.route('/')
+@app.route('/production')
 def times():
     country_retailers = loads(cache.get('scrapper_time_metrics').decode('utf-8'))
+    finished_today = []
+    still_running_today = []
+    has_not_started_today = []
+    for cr_key in country_retailers:
+        country_retailer = country_retailers[cr_key]
+        cr_data = {}
+        cr_data['key'] = cr_key
+        start_time = country_retailer.get('start_time', None)
+        if start_time is None:
+            has_not_started_today.append(cr_data)
+            continue
+        start_time = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+        stop_time = country_retailer.get('stop_time', None)
+        error = country_retailer.get('error', None)
+        cr_data['date'] = str(start_time.date())
+        cr_data['start_time'] = str(start_time.time())
+        if stop_time:
+            stop_time = datetime.datetime.strptime(country_retailer['stop_time'], "%Y-%m-%d %H:%M:%S")
+            cr_data['stop_time'] = str(stop_time.time())
+            cr_data['duration'] = str(stop_time - start_time)
+
+        tz = timezone('Asia/Singapore')
+        current_time = datetime.datetime.strptime(str(datetime.datetime.now(tz))[0:19], "%Y-%m-%d %H:%M:%S")
+        delta_days = (current_time - start_time).days
+        if delta_days < 1:
+            if stop_time:
+                finished_today.append(cr_data)
+            else:
+                still_running_today.append(cr_data)
+        else:
+            has_not_started_today.append(cr_data)
+    return render_template('index2.html', **locals())
+
+@app.route('/staging')
+def times_staging():
+    country_retailers = loads(cache.get('scrapper_time_metrics_demo').decode('utf-8'))
     finished_today = []
     still_running_today = []
     has_not_started_today = []
